be0b4881a899b8d5a3d1006fd113aeb46c987db7
rcontini <rachelecontini5@gmail.com>
2022-10-02 18:56:58 -0400

    created new component and displayed relevant temperature in different units

---
+++ src/WeatherTemperature.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("celsius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  if (unit === "celsius") {
+    return (
+      <span>
+        <span className="temp" id="currentTemp">
+          {Math.round(props.celsius)}
+        </span>
+        <span className="units">
+          <a href="/" id="unitLink">
+            °C |{" "}
+          </a>
+          <a href="/" id="unitLink" onClick={showFahrenheit}>
+            °F
+          </a>
+        </span>
+      </span>
+    );
+  } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32;
+    return (
+      <span>
+        <span className="temp" id="currentTemp">
+          {Math.round(fahrenheit)}
+        </span>
+        <span className="units">
+          <a href="/" id="unitLink" onClick={showCelsius}>
+            °C |{" "}
+          </a>
+          <a href="/" id="unitLink">
+            °F
+          </a>
+        </span>
+      </span>
+    );
+  }
+}
